28f9b8f9bdb6c55ec8511096c2de8dfd567ad11c
Abel Arana <abel.arana@hotmail.com>
2024-07-24 20:47:48 -0500

    cambios

--- public/js/app.js
+++ public/js/app.js
@@ -100275,8 +100275,8 @@ var options = {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\proyectos\segfact2\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\proyectos\segfact2\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\proyectos\segfact2\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\proyectos\segfact2\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
